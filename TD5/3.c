12682a9fcd6789ebbe8588023c38cb1ee32843dc
jessiekrg <jessie.krg@gmail.com>
2025-04-22 10:03:55 +0200

    hihi

--- TD5/3.c
+++ TD5/3.c
@@ -42,8 +42,77 @@ int P2(int m, int n){
     pid_t pid;
     int compteur = 0;
 
-    
+    for (int i = 0; i<m; i++){
+        pid = fork(); //le processus père créait m processus fils
 
+        if (pid < 0){
+            perror("Erreur lors du fork \n");
+            exit(EXIT_FAILURE);
+        
+        } else if (pid == 0){ //le m processus fils créait n processus fils
+            printf("Je suis le processus fils lvl = 1 de PID : %d dont le PID du père est : %d \n", getpid(), getppid());
+            for (int j = 0; j < n; j++){
+                pid = fork();
+
+                if (pid < 0){
+                    perror("Erreur lors du fork \n");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (pid == 0){
+                    printf("Je suis le processus fils lvl 2 de PID : %d dont le PID du père est : %d \n", getpid(), getppid());
+                    exit(EXIT_SUCCESS);
+                }
+
+            }
+
+            for (int j = 0; j < n; j++){ //Attente par le m processus fils la terminaison du n processus fils
+                wait(NULL);
+            }
+
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    for (int i = 0; i<m; i++){
+        wait(NULL);
+    }
+
+    return 0;
+
+}
+
+
+
+int Fonction_recursive(int m, int n, int niveau_max, int *compteur ){
+    //vérification du niveau
+    if (m == niveau_max){
+        return 0;
+    } else {
+        for ( int i = 0; i < n ; i++ ){
+            pid_t pid = fork();
+
+            if (pid == 0){
+                printf("Processus de PID : %d du niveau %d, dont les père est : %d \n", getpid(), m, getppid());
+                Fonction_recursive(m+1, n, niveau_max);
+                exit(1);
+            } else {
+                wait(NULL);
+                compteur++;
+            }
+
+
+        }
+
+    }
+    return compteur;
+}
+
+
+int P3(int m, int n){
+    Fonction_recursive(0,n,m);
+    exit(0);
+    return 0;
 }
 
 int main(int argc, char *argv[]){
@@ -56,7 +125,9 @@ int main(int argc, char *argv[]){
     int m = atoi(argv[1]);
     int n = atoi(argv[2]);
 
-    P1(m,n);
+    //P1(m,n);
+    //P2(m,n);
+    P3(m,n);
 
     return 0;
 
